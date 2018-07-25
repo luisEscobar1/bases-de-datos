fe8303fcdae69bffc222850eb161dbda02bd1a1b
LabingXEON <LabingXEON@LabingXEON5-PC>
2018-07-25 11:20:08 -0500

    clase 1

---
+++ src/bases1/Bases1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bases1;
+
+/**
+ *
+ * @author LabingXEON
+ * @version 0.1
+ * git -hub
+ */
+public class Bases1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola mundo");
+    
+    
+    }
+    
+}
